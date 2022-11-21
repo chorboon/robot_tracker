0f359aa58da70001688b79b1aee5b77f276992c9
James Tan <chorboon@gmail.com>
2022-11-21 12:07:06 +0800

    updates

--- robot_tracker.py
+++ robot_tracker.py
@@ -3,8 +3,11 @@ class Robot:
     def __init__(self):
         self.coords = {"x" :0, "y":0, "d":0}
         print("Robot is online")
+        self.help()
 
     def __call__(self,command):
+        if command == "Q":
+            exit()
         if command == "turn_left":
             self.coords["d"] = (self.coords["d"]-1)% 4
         if command == "turn_right":
@@ -12,30 +15,28 @@ class Robot:
         if command == "move":            
             if self.coords["d"] == 0:
                 self.coords["y"] += 1
-            if self.coords["d"] == 1:
-                self.coords["y"] -= 1
             if self.coords["d"] == 2:
+                self.coords["y"] -= 1
+            if self.coords["d"] == 1:
                 self.coords["x"] += 1
             if self.coords["d"] == 3:
                 self.coords["x"] -= 1
     
     def __del__(self):
         print("Robot is offline")
-            
+    
+    def help(self):
+        print("""Valid commands are:
+        turn_left
+        turn_right
+        move
+        Q
+        
+        """)
 
 curiosity = Robot()
 
-curiosity("turn_left")
-
-curiosity("move")
-
-curiosity("turn_right")
-
-curiosity("move")
-curiosity("move")
-curiosity("move")
-curiosity("turn_right")
-curiosity("move")
-
-curiosity.__del__()
-print(curiosity.coords)
+while 1:
+    command_input = input ("Please give a command to curiousity > ")
+    curiosity(command_input)
+    print(curiosity.coords)
